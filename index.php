cf648779077edf14c1eb4669f0e68a0688000e6c
Joseph Endale <josi.endal45@gmail.com>
2023-01-17 15:57:23 +0300

    Laravel Project

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+define('LARAVEL_START', microtime(true));
+define('ROOT', dirname($_SERVER['SCRIPT_FILENAME']).'/');
+
+
+
+require __DIR__.'/source/vendor/autoload.php';
+
+
+
+$app = require_once __DIR__.'/source/bootstrap/app.php';
+
+
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
+
